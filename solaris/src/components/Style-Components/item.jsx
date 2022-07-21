21f2d03288fe6db6cb3bdc5a911f92c729a5e44d
Ascian <richii_189@hotmail.com>
2022-07-21 00:01:06 -0500

    funcion asincrona de la carga de productos

--- solaris/src/components/Style-Components/item.jsx
+++ solaris/src/components/Style-Components/item.jsx
@@ -16,22 +16,22 @@ export default function Item(props) {
     }
   return (
     <div className="card col-3">
-        <img src={require(`../../img/${props.img}`)} className="card-img-top" alt="..."/>
+        <a href={`#${props.Id}`}><img src={require(`../../img/${props.Img}`)} className="card-img-top" alt="..."/></a>
         <div className="card-body text-center m-2">
             <h5 className="card-title">{props.Nombre}</h5>
             <p className="card-text">{props.Descripcion}</p>
-            <label className="disabled">{props.stock} disponibles</label>
+            <label className="disabled">{props.Stock} disponibles</label>
             <div className="d-flex justify-content-between">
                 <p>{props.Precio}</p>
                 <div className="btn-group" role="group">
-                    <button type="button"className="btn btn-outline-primary" onClick={()=>substract_item(props.stock)}>-</button>
+                    <button type="button"className="btn btn-outline-primary" onClick={()=>substract_item(props.Stock)}>-</button>
                     <label className="btn">{carrito}</label>
-                    <button type="button"className="btn btn-outline-primary" onClick={()=>add_item(props.stock)}>+</button>
+                    <button type="button"className="btn btn-outline-primary" onClick={()=>add_item(props.Stock)}>+</button>
                 </div>
             </div>
-            <div className="mt-2" role="">
-                    <button type="button"className="btn btn-outline-primary">Agregar al carrito</button>
-                </div>
+            <div className="d-flex mt-2 justify-content-end">
+                <button type="button" className="btn btn-outline-primary">Agregar al carrito</button>
+            </div>
         </div>
     </div>
   )
